c62b6ca03c287b6cc165ce048a679fbbbd4f3281
Arif Akmal Bin Kamarudin <152966568+cipherrave@users.noreply.github.com>
2024-03-18 22:52:49 +0800

    init table housecleaning

--- database/tableInit.js
+++ database/tableInit.js
@@ -4,7 +4,7 @@ import pool from "./connection.js";
 export async function createUserTable() {
   try {
     const createTableQuery = await pool.query(
-      'CREATE TABLE IF NOT EXISTS "users" ( user_id VARCHAR(225) UNIQUE NOT NULL PRIMARY KEY, fname VARCHAR(225), lname VARCHAR(225), email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(225) NOT NULL, validation_key VARCHAR(255) NOT NULL UNIQUE, validated BOOLEAN NOT NULL, admin_id VARCHAR(225) UNIQUE, company BOOLEAN, company_name VARCHAR(225), creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP )'
+      'CREATE TABLE IF NOT EXISTS "users" (user_id VARCHAR(225) UNIQUE NOT NULL PRIMARY KEY, fname VARCHAR(225), lname VARCHAR(225), email VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(225) NOT NULL, validation_key VARCHAR(255) NOT NULL UNIQUE, validated BOOLEAN NOT NULL, admin_id VARCHAR(225) UNIQUE, company BOOLEAN, company_name VARCHAR(225), creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)'
     );
     console.log("users table created successfully");
   } catch (error) {
@@ -16,7 +16,7 @@ export async function createUserTable() {
 export async function createVehicleTable() {
   try {
     const createTableQuery = await pool.query(
-      'CREATE TABLE IF NOT EXISTS "vehicle" ( vehicle_id VARCHAR(225) UNIQUE NOT NULL PRIMARY KEY, vname VARCHAR(225) NOT NULL, reg_num VARCHAR(20) NOT NULL, brand VARCHAR(50) NOT NULL, model VARCHAR(225) NOT NULL, purchase_year INTEGER, mileage INTEGER NOT NULL, notes VARCHAR(1000), creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, user_id VARCHAR(225) REFERENCES "users"(user_id) ON DELETE CASCADE )'
+      'CREATE TABLE IF NOT EXISTS "vehicle" (vehicle_id VARCHAR(225) UNIQUE NOT NULL PRIMARY KEY, vname VARCHAR(225) NOT NULL, reg_num VARCHAR(20) NOT NULL, brand VARCHAR(50) NOT NULL, model VARCHAR(225) NOT NULL, purchase_year INTEGER, mileage INTEGER NOT NULL, notes VARCHAR(1000), creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, user_id VARCHAR(225) REFERENCES "users"(user_id) ON DELETE CASCADE)'
     );
     console.log("vehicles table created successfully");
   } catch (error) {
@@ -28,7 +28,7 @@ export async function createVehicleTable() {
 export async function createServiceTable() {
   try {
     const createTableQuery = await pool.query(
-      'CREATE TABLE IF NOT EXISTS "service" ( service_id VARCHAR(225) UNIQUE NOT NULL PRIMARY KEY, next_mileage INTEGER NOT NULL, next_date DATE, cost INTEGER, service_name VARCHAR(255) NOT NULL, place VARCHAR(255), notes VARCHAR(1000), service_date DATE NOT NULL, creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, vehicle_id VARCHAR(225) REFERENCES "vehicle"(vehicle_id), user_id VARCHAR(255))'
+      'CREATE TABLE IF NOT EXISTS "service" (service_id VARCHAR(225) UNIQUE NOT NULL PRIMARY KEY, next_mileage INTEGER NOT NULL, next_date DATE, cost INTEGER, service_name VARCHAR(255) NOT NULL, place VARCHAR(255), notes VARCHAR(1000), service_date DATE NOT NULL, creation_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, vehicle_id VARCHAR(225) REFERENCES "vehicle"(vehicle_id) ON DELETE CASCADE)'
     );
     console.log("service table created successfully");
   } catch (error) {
